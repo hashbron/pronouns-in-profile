81245e64f5ab353413421761a7e43f5141961acc
Harrison Bronfeld <harrisonbronfeld@Harrisons-Air.fios-router.home>
2021-02-04 18:36:18 -0500

    Added additonal url checks to only look for pronouns on profile pages

--- check-for-pronoun.js
+++ check-for-pronoun.js
@@ -1,7 +1,16 @@
-waitForElm(PROFILE_ELEMENTS_LIST_SELECTOR).then( // Wait for profile to load
-	elm => {
-		removePronounElementIfPresent(elm); // Remove a cached pronoun object from the profile
-		changeText(document.querySelector(BIO_SELECTOR)); // Add new pronoun object from bio
-});
+var title = document.getElementsByTagName("title")[0];
+
+// Check that the page title has a "@" to confirm it is a user profile page
+// This distinguishes from pages like "twitter.com/home" and "twitter.com/notifcations"
+if (title.textContent.includes("@")){ 
+
+	waitForElm(PROFILE_ELEMENTS_LIST_SELECTOR).then( // Wait for profile to load
+		elm => {
+			removePronounElementIfPresent(elm); // Remove a cached pronoun object from the profile
+			changeText(document.querySelector(BIO_SELECTOR)); // Add new pronoun object from bio
+	});
+}
+
+
 
 
