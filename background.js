998df2c6d63ed0c54aebf522861efd2757a54c42
Harrison Bronfeld <harrisonbronfeld@Harrisons-Air.fios-router.home>
2021-02-04 18:35:49 -0500

    New event listener to watch for url changes

--- background.js
+++ background.js
@@ -1,4 +1,20 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-	chrome.tabs.executeScript(null, {file:"global.js"});
-    chrome.tabs.executeScript(null, {file:"check-for-pronoun.js"});
+url ="";
+
+chrome.tabs.onUpdated.addListener(function
+  (tabId, changeInfo, tab) {
+
+    // Only update the page if the url has changed
+    if (changeInfo.url && url != changeInfo.url) {
+
+    	url = changeInfo.url; // Reset saves url to updated url
+
+    	// Only procede if current url matches the regex for a twitter profile "https://twitter.com/username"
+    	var urlRegex = /^http(s|):\/\/twitter.com\/(\w+)(\/|)$/g;
+    	if (urlRegex.test(url)) {
+
+			chrome.tabs.executeScript(null, {file:"global.js"});
+			chrome.tabs.executeScript(null, {file:"check-for-pronoun.js"});
+		}
+    }
+
 });
